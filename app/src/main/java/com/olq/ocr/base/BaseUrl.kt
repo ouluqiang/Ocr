224222ed0737c258c2146f7d6bd91ee2829ddf5b
olq <632521056@qq.com>
2020-11-24 23:25:50 +0800

    链接

--- app/src/main/java/com/olq/ocr/base/BaseUrl.kt
+++ app/src/main/java/com/olq/ocr/base/BaseUrl.kt
@@ -1,16 +1,19 @@
 package com.olq.ocr.base
 
-import rxhttp.wrapper.annotation.DefaultDomain
 
 /**
  * create 2020/11/23 17:50
  *  create admin
  **/
-class BaseUrl {
+object BaseUrl {
 
 //    @JvmField
 //    @DefaultDomain //设置为默认域名
 //    val baseUrl = "https://api.rubymmm.com/api/"
 //    val CATEGORY="Client/Category";
+
+    val API_KEY="8T50DeZoej5d6wFbTehiS3ZP"
+    val SECRET_KEY="LDmFZcAFjR3sv7LHDy6PntnFSVljkWW3"
+    val base="https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=$API_KEY&client_secret=$SECRET_KEY"
 
 }
